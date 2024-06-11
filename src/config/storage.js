f1f726910656540f5c93caedd184fcd082c5f068
Bulan Jatuh <lutfisobri@gmail.com>
2024-06-11 12:45:02 +0700

    fix: bucket name

--- src/config/storage.js
+++ src/config/storage.js
@@ -1,7 +1,7 @@
 const { Storage } = require('@google-cloud/storage');
 
 const storage = new Storage();
-const bucketName = process.env.BUCKET_NAME;
+const bucketName = process.env.BUCKET_NAME || "soca-app";
 const bucket = storage.bucket(bucketName);
 
 module.exports = bucket;
