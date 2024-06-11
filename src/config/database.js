94473358caadef3644f8724601ae64f996650994
Bulan Jatuh <lutfisobri@gmail.com>
2024-06-11 13:08:42 +0700

    fix: config database for server

--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,7 @@ const mssql = require('mssql');
 const connection = async config => {
   const dbConfig = {
     server: process.env.DB_SERVER,
-    port: parseInt(process.env.DB_PORT),
+    // port: parseInt(process.env.DB_PORT),
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
